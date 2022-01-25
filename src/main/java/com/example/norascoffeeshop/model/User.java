0bdd3b8b8de8e54e16a38e0790f9b0f9ddcc3099
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-01-25 13:52:39 +0200

    resources

--- src/main/java/com/example/norascoffeeshop/model/User.java
+++ src/main/java/com/example/norascoffeeshop/model/User.java
@@ -1,6 +1,8 @@
 package com.example.norascoffeeshop.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
@@ -17,6 +19,9 @@ public class User  extends AbstractPersistable<Long>{
     private String name;
     private String address;
     private String phonenumber;
+    @Column(unique = true)
+    @Email
+    private String email;
     private String password;
-    private Boolean admin;
+    private Boolean isAdmin;
 }
