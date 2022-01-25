c5c86d42d08811f5075c7450a9ab9ceb0c2cd11c
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-01-25 10:43:35 +0200

    start

---
+++ src/main/java/com/example/norascoffeeshop/NorascoffeeshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.norascoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorascoffeeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorascoffeeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/norascoffeeshop/NorascoffeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.norascoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorascoffeeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
