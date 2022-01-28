357d77b47401d7f84254db9019be47fef9b0abfb
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-01-28 12:00:01 +0200

    makerService

--- src/main/java/com/example/norascoffeeshop/service/MakerService.java
+++ src/main/java/com/example/norascoffeeshop/service/MakerService.java
@@ -1,5 +1,8 @@
 package com.example.norascoffeeshop.service;
 
+import java.util.List;
+
+import com.example.norascoffeeshop.model.Maker;
 import com.example.norascoffeeshop.repository.MakerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,5 +13,31 @@ public class MakerService {
 
     @Autowired
     private MakerRepository makerRepository;
+
+    public List<Maker> listAll(){
+        return makerRepository.findAll();
+    }
+
+    public Maker getEditor(Long id){
+        return makerRepository.getById(id);
+    }
+
+    public void addMaker(String name, String url){
+        Maker maker = new Maker();
+        maker.setName(name);
+        maker.setUrl(url);
+        makerRepository.save(maker);
+    }
+
+    public void updateMaker(Long id, String name, String url){
+        Maker maker = makerRepository.getById(id);
+        maker.setName(name);
+        maker.setUrl(url);
+        makerRepository.save(maker);
+    }
+    public void deleteEditor(Long id){
+        makerRepository.deleteById(id);
+    }
+
     
 }
