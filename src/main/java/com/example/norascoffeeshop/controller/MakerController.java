63e98f0182f16cf933ab7b2857d91037bb0f5aa3
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-01-31 13:47:12 +0200

    maker

--- src/main/java/com/example/norascoffeeshop/controller/MakerController.java
+++ src/main/java/com/example/norascoffeeshop/controller/MakerController.java
@@ -1,8 +1,23 @@
 package com.example.norascoffeeshop.controller;
 
+import com.example.norascoffeeshop.service.MakerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MakerController {
+
+    @Autowired
+    private MakerService makerService;
+
+    @GetMapping("/maker")
+    public String getAllMakers(Model model){
+        model.addAttribute("makers", this.makerService.listAll());
+        return "admin";
+        
+    }
     
 }
