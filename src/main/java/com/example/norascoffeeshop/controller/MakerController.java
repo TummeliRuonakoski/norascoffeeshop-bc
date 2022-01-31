357d74ec725da5c8356da7fa45c869ce5b499a9d
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-01-31 14:04:18 +0200

    makercontroller

--- src/main/java/com/example/norascoffeeshop/controller/MakerController.java
+++ src/main/java/com/example/norascoffeeshop/controller/MakerController.java
@@ -5,7 +5,11 @@ import com.example.norascoffeeshop.service.MakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MakerController {
@@ -13,11 +17,34 @@ public class MakerController {
     @Autowired
     private MakerService makerService;
 
-    @GetMapping("/maker")
+    @GetMapping("admin/maker")
     public String getAllMakers(Model model){
         model.addAttribute("makers", this.makerService.listAll());
         return "admin";
-        
     }
-    
+
+    @GetMapping("admin/maker/{id}")
+    public String getMaker(@PathVariable Long id, Model model){
+        model.addAttribute("maker", makerService.getMaker(id));
+        return "maker";
+    }
+
+    @PostMapping("admin/maker")
+    public String createMaker(@RequestParam String name, @RequestParam String url){
+        this.makerService.addMaker(name, url);
+        return "redirect:/admin";
+
+    }
+
+    @PostMapping("admin/maker/{id}")
+    public String updateMaker(@PathVariable Long id, @RequestParam String name, @RequestParam String url){
+        this.makerService.updateMaker(id, name, url);
+        return "redirect:/maker";
+    }
+
+    @DeleteMapping("admin/maker/{id}")
+    public String deleteMaker(@PathVariable Long id){
+        this.makerService.deleteMaker(id);
+        return "redirect:/admin";
+    }    
 }
